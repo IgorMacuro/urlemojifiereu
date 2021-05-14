b1c49a010125ad6af1814d3eea250f834d5c336d
clover <tclover@tutanota.com>
2021-05-14 16:37:23 +0200

    fix autowired

--- src/main/java/com/backend/emojifier/encoders/EmojiEncoder.java
+++ src/main/java/com/backend/emojifier/encoders/EmojiEncoder.java
@@ -12,7 +12,11 @@ import com.vdurmont.emoji.EmojiManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.stereotype.Service;
 
+@Service
+@Configurable
 public class EmojiEncoder {
     private Url url;
     private int attempts_num = 10;
