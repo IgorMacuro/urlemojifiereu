ac011240793bf4e7f4c7a6549ea177ffbe424bbc
clover <tclover@tutanota.com>
2021-05-14 12:10:29 +0200

    initial commit

---
+++ src/main/java/com/backend/emojifier/EmojifierApplication.java
@@ -0,0 +1,13 @@
+package com.backend.emojifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmojifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmojifierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/emojifier/EmojifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.emojifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmojifierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
