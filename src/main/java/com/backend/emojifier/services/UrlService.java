f6cb5ae17955d38030c5605b3d8f975cbe11f6ef
clover <tclover@tutanota.com>
2021-05-14 19:56:16 +0200

    change foundbyencoded

--- src/main/java/com/backend/emojifier/services/UrlService.java
+++ src/main/java/com/backend/emojifier/services/UrlService.java
@@ -1,15 +1,19 @@
 package com.backend.emojifier.services;
 
-import java.util.List;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 
 import com.backend.emojifier.entities.Url;
 import com.backend.emojifier.repositories.UrlRepository;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UrlService {
+    public static final Logger log = LoggerFactory.getLogger(UrlService.class);
     @Autowired
     private UrlRepository urlRepository;
 
@@ -20,7 +24,11 @@ public class UrlService {
         return urlRepository.save(url);
     }
 
-    public Url findByEncodedUrl(String encodedUrl){
-        return urlRepository.findByEncodedUrl(encodedUrl);
+    public Url findByEncodedUrl(String encodedUrl) throws UnsupportedEncodingException{
+        String decoded = URLDecoder.decode(encodedUrl, "UTF-8");
+        log.info("decoded url: " + decoded);
+        Url found = urlRepository.findByEncodedUrl(decoded);
+        log.info("Plain url found: " + found.toString());
+        return found;
     }
 }
